239358c8ce7f849b79b49fb378ea886a222123ca
dev-kant-kumar <dev.mytechhandle@gmail.com>
2024-08-30 19:54:21 +0530

    Working on it

--- College Class Work/Assigned Problem/Billing/1.cpp
+++ College Class Work/Assigned Problem/Billing/1.cpp
@@ -8,51 +8,81 @@
 // 4 - displaying the entire list
 
 #include <iostream>
+#include <iomanip>
 #include <string>
 using namespace std;
-
-void UI();
 
 class Product
 {
 
-private:
+public:
     int product_code;
     string product_name;
     double product_price;
 
-public:
-    Product(int productCode, string productName, double productPrice)
+    void addProduct(int productCode, string productName, float productPrice)
     {
-        this->product_code = productCode;
-        this->product_name = productName;
-        this->product_price = productPrice;
+        product_code = productCode;
+        product_name = productName;
+        product_price = productPrice;
     }
 };
 
+void displayAvailableProducts(Product items[]);
+void UI();
+void addItem();
+
 int main()
 {
+    Product item[5];
+    item[0].addProduct(101, "101A", 100);
+    item[1].addProduct(102, "102A", 200);
+    item[2].addProduct(103, "103A", 300);
+    item[3].addProduct(104, "104A", 400);
+    item[4].addProduct(105, "105A", 360);
+    item[5].addProduct(106, "106A", 1300);
+
+    displayAvailableProducts(item);
+
     UI();
+
     return 0;
+}
+
+void displayAvailableProducts(Product items[])
+{
+
+    cout << "\n\n\033[1;34mWelcome to ABC Store\033[0m\n\n";
+    cout << "\033[1;32m  These item are available in stocks \033[0m\n";
+    cout << "_______________________________________________\n";
+    cout << left; // Left-align the text
+    cout << "| " << setw(15) << "\033[1;33mProduct Code\033[0m" << " | " << setw(15) << "\033[1;33mProduct Name\033[0m" << " | " << setw(15) << "\033[1;33mProduct Price\033[0m" << " |" << endl;
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "| " << setw(12) << items[i].product_code << " | " << setw(12) << items[i].product_name << " | " << setw(13) << items[i].product_price << " |\n";
+    }
+    cout << "_______________________________________________\n";
+
+    cout << "\n\n";
 }
 
 void UI()
 {
     int choice;
 
-    cout << "Welcome to \n";
+    string options = " [1] - Add an item \n [2] - Delete an item \n [3] - Check item's total value \n [4] - See complete items list \n";
 
-    string options = " [1] - Add an item \n [2] - Delete an item \n [3] - Check item's total value \n [4] - See complete items list \n\n";
-
+    cout << "\033[1;35mSelect from these options for your order cart \033[0m\n\n";
     cout << options << endl;
-    cout << "Enter your choice {like 1} : ";
+    cout << "\033[1;36mEnter your choice {like 1} : \033[0m";
     if (cin >> choice)
     {
         switch (choice)
         {
         case 1:
             cout << "Adding item selected\n";
-            // addItem();
+            addItem();
             break;
         case 2:
             cout << "Deleting item selected\n";
@@ -66,11 +96,15 @@ void UI()
             cout << "See complete items list selected\n";
             break;
         default:
-            cout << "Invalid options entered! Enter from 1 to 4 only\n";
+            cout << "\033[5;31mInvalid options entered! Enter from 1 to 4 only\033[0m\n";
         }
     }
     else
     {
-        cout << "Invalid input! Only numbers are allowed.\n";
+        cout << "\033[5;31mInvalid input! Only numbers are allowed.\033[0m\n";
     }
+}
+
+void addItem()
+{
 }
