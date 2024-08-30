94afb824b317b7144d4797cae37bdf58a1345cb6
dev-kant-kumar <dev.mytechhandle@gmail.com>
2024-08-30 11:50:55 +0530

    Problem statement added

---
+++ College Class Work/Assigned Problem/Billing/1.cpp
@@ -0,0 +1,8 @@
+// Problem Statement
+// Create a class to manage every month item list we place to a dealer
+// list include -> item code,price of each item
+// performing following operations
+// 1 - Adding an item
+// 2 - Deleting a item
+// 3 - printing total value
+// 4 - displaying the entire list
