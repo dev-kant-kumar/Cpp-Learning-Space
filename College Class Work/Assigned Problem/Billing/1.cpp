d0fc2a041b06368ed23b23260df137dbb04ffda7
dev-kant-kumar <dev.mytechhandle@gmail.com>
2024-08-31 21:36:35 +0530

    addItem functionality added

--- College Class Work/Assigned Problem/Billing/1.cpp
+++ College Class Work/Assigned Problem/Billing/1.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <windows.h>
 using namespace std;
 
 class Product
@@ -28,29 +29,36 @@ public:
     }
 };
 
-void displayAvailableProducts(Product items[]);
-void UI();
-void addItem();
+void addProduct();               // add product in store
+void displayAvailableProducts(); // display available products
+void UI();                       // display options available to perform on item list
+void addItem();                  // add item in item list
+
+Product item[5];       // store is each product in store
+Product itemList[100]; // store the product added to item list
+int noOfItemInList = 0;
 
 int main()
 {
-    Product item[5];
+    addProduct();
+    UI();
+
+    return 0;
+}
+
+void addProduct()
+{
     item[0].addProduct(101, "101A", 100);
     item[1].addProduct(102, "102A", 200);
     item[2].addProduct(103, "103A", 300);
     item[3].addProduct(104, "104A", 400);
     item[4].addProduct(105, "105A", 360);
     item[5].addProduct(106, "106A", 1300);
-
-    displayAvailableProducts(item);
-
-    UI();
-
-    return 0;
 }
 
-void displayAvailableProducts(Product items[])
+void displayAvailableProducts()
 {
+    system("cls");
 
     cout << "\n\n\033[1;34mWelcome to ABC Store\033[0m\n\n";
     cout << "\033[1;32m  These item are available in stocks \033[0m\n";
@@ -60,7 +68,7 @@ void displayAvailableProducts(Product items[])
 
     for (int i = 0; i < 5; i++)
     {
-        cout << "| " << setw(12) << items[i].product_code << " | " << setw(12) << items[i].product_name << " | " << setw(13) << items[i].product_price << " |\n";
+        cout << "| " << setw(12) << item[i].product_code << " | " << setw(12) << item[i].product_name << " | " << setw(13) << item[i].product_price << " |\n";
     }
     cout << "_______________________________________________\n";
 
@@ -73,6 +81,8 @@ void UI()
 
     string options = " [1] - Add an item \n [2] - Delete an item \n [3] - Check item's total value \n [4] - See complete items list \n";
 
+    displayAvailableProducts();
+MainMenu:
     cout << "\033[1;35mSelect from these options for your order cart \033[0m\n\n";
     cout << options << endl;
     cout << "\033[1;36mEnter your choice {like 1} : \033[0m";
@@ -81,30 +91,100 @@ void UI()
         switch (choice)
         {
         case 1:
-            cout << "Adding item selected\n";
+            cout << "\n";
+            cout << setw(55) << "Adding item selected" << "Order List have : " << noOfItemInList << " items\n";
+            cout << "-----------------------------------------------------------------------------------";
             addItem();
             break;
         case 2:
-            cout << "Deleting item selected\n";
+            cout << "\nDeleting item selected\n";
+            cout << "-----------------------------------------------------------------------------------";
             // deleteItem();
             break;
         case 3:
-            cout << "Check item's total value selected\n";
+            cout << "\nCheck item's total value selected\n";
+            cout << "-----------------------------------------------------------------------------------";
             break;
 
         case 4:
-            cout << "See complete items list selected\n";
+            cout << "\nSee complete items list selected\n";
+            cout << "-----------------------------------------------------------------------------------";
             break;
         default:
-            cout << "\033[5;31mInvalid options entered! Enter from 1 to 4 only\033[0m\n";
+            Beep(750, 100);
+            cout << "\n\033[5;31mInvalid options entered! Enter from 1 to 4 only\033[0m\n";
+            goto MainMenu;
         }
     }
     else
     {
-        cout << "\033[5;31mInvalid input! Only numbers are allowed.\033[0m\n";
+        Beep(750, 100);
+        cout << "\n\033[5;31mInvalid input! Only numbers are allowed.\033[0m\n";
     }
 }
 
 void addItem()
 {
+addItemMenu:
+    int choice;
+
+    cout << "\n\033[1;35mSelect from these options \033[0m\n\n";
+    cout << "Enter 0 to exit and back to main menu \nEnter 1 to continue\n\n";
+    cout << "\033[1;36mEnter your choice {like 1} : \033[0m";
+
+    if (cin >> choice)
+    {
+        if (choice == 0)
+        {
+            cout << "\nBack to Main Menu\n";
+            UI();
+        }
+        else if (choice == 1)
+        {
+        addItem:
+            // adding item in item list
+            int product_code;
+            cout << "\n\033[1;36mEnter product code : \033[0m";
+            cin >> product_code;
+
+            // check if the product exist with entered product code
+            bool matched = true;
+            for (int i = 0; i < 5; i++)
+            {
+                if (item[i].product_code == product_code)
+                {
+                    itemList[noOfItemInList].product_code = item[i].product_code;
+                    itemList[noOfItemInList].product_name = item[i].product_name;
+                    itemList[noOfItemInList].product_price = item[i].product_price;
+                    noOfItemInList++;
+
+                    cout << "\n"
+                         << itemList[noOfItemInList - 1].product_name << setw(60) << " \033[1;32mAdded to your order list\033[0m" << "\033[1;32mTotal item is order list : \033[0m" << noOfItemInList << "\n";
+                    cout << "-----------------------------------------------------------------------------------";
+
+                    goto addItemMenu;
+                }
+                else
+                {
+                    matched = false;
+                }
+            }
+
+            if (matched == false)
+            {
+                cout << "\n\033[5;31mEnter valid product code to add item \033[0m\n";
+                goto addItem;
+            }
+        }
+        else
+        {
+            cout << "\n\033[5;31mInvalid Input! Enter 0 or 1 only\033[0m\n";
+            goto addItemMenu;
+        }
+    }
+    else
+    {
+        cout << "\n\033[5;31mOnly numbers are allowed \033[0m\n";
+        // goto addItemMenu;
+    }
 }
